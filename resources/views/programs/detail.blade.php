f99b56f0a2b70be501e12aa23dc13d9c17952c7a
Fikri Yusrihan <47127883+fikriyusrihan@users.noreply.github.com>
2021-11-30 18:56:46 +0700

    Create Record Model

--- resources/views/programs/detail.blade.php
+++ resources/views/programs/detail.blade.php
@@ -8,7 +8,7 @@
         <div class="menu-section row">
             <div class="featured-card">
                 <div class="row justify-content-center">
-                    <div class="col-lg-10 col-12">
+                    <div class="col-lg-12 col-12 px-5">
                         <div class="category-card mt-5">
                             <img src="{{ asset('assets/images/program/poster/' . $program->poster) }}" alt=""
                                 width="100%" />
@@ -19,7 +19,7 @@
         </div>
         <!-- keterangan program -->
         <div class="menu-section row mt-5">
-            <div class="description col-lg-6 col-sm-12 mt-5 mx-auto">
+            <div class="description col-lg-8 col-sm-12 mt-5 mx-auto ps-5">
                 <p class="content-description">
                     {{ $program->description }}
                 </p>
@@ -29,6 +29,18 @@
                     <img src="{{ asset('assets/images/download.png') }}" alt="" width="40%" />
                 </center>
             </div>
+        </div>
+
+        {{-- Checklist --}}
+        <div class="row mt-5 px-5">
+            <h6 class="text-center">Checklists</h6>
+                @if ($program->category->id == 1)
+                    <p>hello</p>
+                @elseif ($program->category->id == 2)
+                    <p>hi</p>
+                @else
+                    <p>gatau</p>
+                @endif
         </div>
     </div>
 @endsection
