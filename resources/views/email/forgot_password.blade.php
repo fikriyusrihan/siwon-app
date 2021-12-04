dec65aed9350715d16c09ef787154de60b9f0594
Fikri Yusrihan <47127883+fikriyusrihan@users.noreply.github.com>
2021-12-04 19:39:59 +0700

    Update Email

--- resources/views/email/forgot_password.blade.php
+++ resources/views/email/forgot_password.blade.php
@@ -1,4 +1,4 @@
-<h1>Forget Password Email</h1>
-   
-You can reset password from bellow link:
+<h1>SiWON Reset Password</h1>
+
+Silakan menuju link berikut untuk mengganti password:
 <a href="{{ route('reset.password.get', $token) }}">Reset Password</a>
