2709e464c81ef9834c6c0cde36fbda42825713ea
Fikri Yusrihan <47127883+fikriyusrihan@users.noreply.github.com>
2021-12-04 17:20:16 +0700

    Forgot Password

---
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+
+class ForgotPasswordController extends Controller
+{
+    public function index() {
+        return view('auth/forgot-password', [
+            'active' => '',
+        ]);
+    }
+
+    public function forgot(Request $request) {
+        $request->validate([
+            'email' => 'email|required',
+        ]);
+
+        $token = Str::random(64);
+
+        DB::table('password_resets')->insert([
+            'email' => $request->email,
+            'token' => $token,
+            'created_at' => Carbon::now(),
+        ]);
+
+        Mail::send('email.forgot_password', ['token' => $token], function($message) use ($request) {
+            $message->to($request->email);
+            $message->subject('Reset Password');
+        });
+
+        return back();
+    }
+}
